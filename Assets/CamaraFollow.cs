6ca26f6d1e4b866c921bc7a2d5915f8fb51e4daf
tinesone <tlr3@live.nl>
2018-02-05 09:51:22 +0100

    Began working on a camara follow

---
+++ Assets/CamaraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour {
+	public GameObject player;
+	Control control;
+
+	// Use this for initialization
+	void Start () {
+		control = player.GetComponent<Control> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)player.transform.position, control.speed * Time.deltaTime);
+		transform.position = new Vector3(pos.x, pos.y, -1);
+	}
+}
