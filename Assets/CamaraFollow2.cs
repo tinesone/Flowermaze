9c8af99641e814f94500cfe5a22a794ad8896b57
tinesone <tlr3@live.nl>
2018-02-07 10:30:57 +0100

    Used MoveTowards instead of Lerp
    Also renamed and merged wifhtOffset and heigtOffset into
    triggerDistance.

--- Assets/CamaraFollow2.cs
+++ Assets/CamaraFollow2.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class CamaraFollow2 : MonoBehaviour {
 	public GameObject player;
 	Control control;
-	public float widthOffset = 1;
-	public float heigtOffset = 1;
+	float triggerDistance = 3;
 
 	void Start () {
 		control = player.GetComponent<Control> ();
@@ -15,8 +14,8 @@ public class CamaraFollow2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 offSet = new Vector2 (player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
-		if (offSet.x < widthOffset * -1 || offSet.x > widthOffset || offSet.y < heigtOffset * -1 || offSet.y > heigtOffset) {
-			Vector2 pos = Vector2.Lerp((Vector2)transform.position, (Vector2)player.transform.position, control.speed * Time.deltaTime); 
+		if (offSet.x < triggerDistance * -1 || offSet.x > triggerDistance || offSet.y < triggerDistance * -1 || offSet.y > triggerDistance) {
+			Vector2 pos = Vector2.MoveTowards((Vector2)transform.position, (Vector2)player.transform.position, control.speed * Time.deltaTime); 
 			transform.position = new Vector3 (pos.x, pos.y, -1);
 		}
 	}
