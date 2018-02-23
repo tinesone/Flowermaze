8fc0539084d91486f32cab26e00b967149774311
tinesone <tlr3@live.nl>
2018-02-23 13:20:40 +0100

    Changed type of combat meganic

--- Assets/Container.cs
+++ Assets/Container.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tempEnemyMovement : MonoBehaviour {
+public class Container : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
