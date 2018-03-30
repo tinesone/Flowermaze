a3acc151086ff654fb4e66aaccd016df1360f7a1
tinesone <tlr3@live.nl>
2018-03-30 11:27:06 +0200

    Moved From tiles to cooördinates

---
+++ Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour {
+
+
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = player.transform.position.x;
+        float y = player.transform.position.y;
+        print(x.ToString("R") + y.ToString("R"));
+	}
+}
