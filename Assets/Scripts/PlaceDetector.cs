e30db7f13c37c518bd7547c6a3d75dfaac6c01eb
tinesone <tlr3@live.nl>
2018-03-30 10:02:45 +0200

    Started Working on a way to place tiles

---
+++ Assets/Scripts/PlaceDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detect : MonoBehaviour {
+
+    public GameObject Tile;
+
+
+	// Use this for initialization
+	void Start () {
+        for (int vector = 1; vector <= 100; vector++) {
+            Instantiate(Tile, new Vector3(vector, 0, 0), transform.rotation);
+            Instantiate(Tile, new Vector3(0, vector, 0), transform.rotation);
+            Instantiate(Tile, new Vector3(vector * -1, 0, 0), transform.rotation);
+            Instantiate(Tile, new Vector3(0, vector * -1, 0), transform.rotation);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
