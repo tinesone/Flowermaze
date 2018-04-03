c66d1d8829e8afe2231837a7585145e745f4accf
tinesone <tlr3@live.nl>
2018-04-03 11:23:14 +0200

    Added a way for the player to be detected in a area

---
+++ Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour {
+
+
+
+    GameObject player;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () {
+        Control control = player.GetComponent<Control>();
+        float x = player.transform.position.x;
+        float y = player.transform.position.y;
+        if (x >= -3.5 && x <= 3.5 && y >= -3.15 && y <= 3.15)
+        {
+            control.ApplyDamage(20);
+        }
+	}
+}
+
+
+/*
+void Earbuds(stupid person)
+{
+    Destroy(person);
+}
+
+Earbuds(mees);
+
+*/
