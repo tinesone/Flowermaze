1dcbc21efe435b3c5cb0719fc1830abef1b79658
tinesone <tlr3@live.nl>
2018-04-03 13:33:43 +0200

    Added a basic music if player enters area

--- Assets/Scripts/DetectPlayer.cs
+++ Assets/Scripts/DetectPlayer.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(AudioSource))]
 public class DetectPlayer : MonoBehaviour {
 
+    AudioSource audioSource;
 
+    private bool played = false;
+
+    public AudioClip music1;
 
     GameObject player;
 
 	void Start () {
+        audioSource = GetComponent<AudioSource>();
         player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void Update () {
+    
         Control control = player.GetComponent<Control>();
         float x = player.transform.position.x;
         float y = player.transform.position.y;
-        if (x >= -3.5 && x <= 3.5 && y >= -3.15 && y <= 3.15)
+        if (x >= -3.5 && x <= 3.5 && y >= -3.5 && y <= 3.5 && played == false)
         {
-            control.ApplyDamage(20);
+            audioSource.PlayOneShot(music1, 1f);
+            played = true;
+
         }
 	}
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,45 +5,48 @@ using UnityEngine.Playables;
 
 
 [RequireComponent(typeof(AudioSource))]
-public class Enemy : MonoBehaviour {
-	
-	public int armor = 30;
+public class Enemy : MonoBehaviour
+{
 
-	public float maxHealth = 200f;
-	private float curHealth = 0;
-	private bool died = false;
-	
-	public GameObject player;
-	
-	public AudioClip death;
-	public AudioClip hit;
-	AudioSource audioSource;
-	
-	void Start(){
-		audioSource = GetComponent<AudioSource>();
-		curHealth = maxHealth;
+    public int armor = 30;
 
-	}
+    public float maxHealth = 200f;
+    private float curHealth = 0;
+    private bool died = false;
 
-	void Update(){
-		if (curHealth <= 0 && died  == false) {
-			died = true;
-			print ("died");
-			audioSource.PlayOneShot(death, 1f);
-			Invoke("remove", 0.4f);
-		}
-	}
+    public GameObject player;
+
+    public AudioClip death;
+    public AudioClip hit;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        curHealth = maxHealth;
+
+    }
 
 
-	public void ApplyDamage(float dmg){
-		dmg = dmg - dmg * armor / 100f;
-		curHealth -= dmg;
-		print(curHealth);
-		//audioSource.PlayOneShot(hit, 1f);
-	}
-
-
-	void remove(){
-		Destroy (gameObject);
-	}
+    public void ApplyDamage(float dmg)
+    {
+        dmg = dmg - dmg * armor / 100f;
+        curHealth -= dmg;
+        print(curHealth);
+        if (curHealth > 15)
+        {
+            print(curHealth);
+            audioSource.PlayOneShot(hit, 1f);
+        }
+        else if (curHealth <= 15 && curHealth >= 0 && died == false)
+        {
+            died = true;
+            print("died");
+            audioSource.PlayOneShot(death, 1f);
+        }
+        else if (curHealth <= 0 && died == true)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
