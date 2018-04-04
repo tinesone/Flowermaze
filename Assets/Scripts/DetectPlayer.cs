a455348217d383d33f63302324e84394e3065029
tinesone <tlr3@live.nl>
2018-04-04 10:23:26 +0200

    Added other (test)music

--- Assets/Scripts/DetectPlayer.cs
+++ Assets/Scripts/DetectPlayer.cs
@@ -8,29 +8,59 @@ public class DetectPlayer : MonoBehaviour {
 
     AudioSource audioSource;
 
-    private bool played = false;
 
     public AudioClip music1;
+    public AudioClip music2;
+    public AudioClip music3;
+
+    private int musicPicker;
+    private int musicBlocker;
 
     GameObject player;
 
 	void Start () {
         audioSource = GetComponent<AudioSource>();
         player = GameObject.FindGameObjectWithTag("Player");
+        musicBlocker = 0;
 	}
 	
 	void Update () {
-    
-        Control control = player.GetComponent<Control>();
         float x = player.transform.position.x;
         float y = player.transform.position.y;
-        if (x >= -3.5 && x <= 3.5 && y >= -3.5 && y <= 3.5 && played == false)
+        if (x >= -3.5 && x <= 3.5 && y >= -3.5 && y <= 3.5)
+            musicPicker = 1;
+        else if (x >= 3.5 && x <= 7 && y <= 7 && y >= 3.5)
+            musicPicker = 2;
+        else if (x >= -3.5 && x < - -7 && y <= -3.5 && y >= -7)
+            musicPicker = 3;
+        switch (musicPicker)
         {
-            audioSource.PlayOneShot(music1, 1f);
-            played = true;
-
+            case (1):
+                if (musicBlocker != musicPicker)
+                {
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(music1, 1f);
+                }
+                musicBlocker = musicPicker;
+                break;
+            case (2):
+                if (musicBlocker != musicPicker)
+                {
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(music2, 1f);
+                }
+                musicBlocker = musicPicker;
+                break;
+            case (3):
+                if (musicBlocker != musicPicker)
+                {
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(music3, 1f);
+                }
+                musicBlocker = musicPicker;
+                break;
         }
-	}
+    }
 }
 
 
