88d65dc0a6e2f415a767a7f3ef3d7ee4485d4ece
Jetse Verschuren <jetse@verschurendegroot.nl>
2018-04-30 19:39:03 +0200

    Fixes #11

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,12 @@ public class Enemy : MonoBehaviour
     public float maxHealth = 200f;
     public AudioClip death;
     public AudioClip hit;
-    public GameObject player;
+    public AudioSource audioSource;
 
-    private float curHealth = 0;
-    private bool died = false;
-    private AudioSource audioSource;
+    private float curHealth;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         curHealth = maxHealth;
     }
 
@@ -24,19 +21,15 @@ public class Enemy : MonoBehaviour
         dmg -= dmg * armor / 100f;
         curHealth -= dmg;
         print(curHealth);
-        if (curHealth > 15)
+        if (curHealth > 0)
         {
             print(curHealth);
             audioSource.PlayOneShot(hit, 1f);
         }
-        else if (curHealth <= 15 && curHealth >= 0 && died == false)
+        else
         {
-            died = true;
             print("died");
             audioSource.PlayOneShot(death, 1f);
-        }
-        else if (curHealth <= 0 && died == true)
-        {
             Destroy(gameObject);
         }
     }
