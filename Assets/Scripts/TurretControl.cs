f2d59267daac882f39e4c2858df2537936eee317
tinesone <tlr3@live.nl>
2018-05-17 11:29:19 +0200

    Cleaned up some work
    Forgot (Yeah i know, Again)

--- Assets/Scripts/TurretControl.cs
+++ Assets/Scripts/TurretControl.cs
@@ -4,29 +4,6 @@ using UnityEngine;
 
 public class TurretControl : MonoBehaviour
 {
-
-    /*void Update()
-    {
-        Control control = this.transform.parent.GetComponent<Control>();
-        float y = Input.GetAxisRaw("Vertical") * control.speed;
-        float x = Input.GetAxisRaw("Horizontal") * control.speed;
-        if (x > 0 && y == 0)
-            transform.rotation = Quaternion.Euler(0, 0, -90);
-        if (x < 0 && y == 0)
-            transform.rotation = Quaternion.Euler(0, 0, 90);
-        if (x == 0 && y > 0)
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        if (x == 0 && y < 0)
-            transform.rotation = Quaternion.Euler(0, 0, 180);
-        if (x > 0 && y > 0)
-            transform.rotation = Quaternion.Euler(0, 0, -45);
-        if (x < 0 && y < 0)
-            transform.rotation = Quaternion.Euler(0, 0, 135);
-        if (x > 0 && y < 0)
-            transform.rotation = Quaternion.Euler(0, 0, -135);
-        if (x < 0 && y > 0)
-            transform.rotation = Quaternion.Euler(0, 0, 45);
-    }*/
     void Update()
     {
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
